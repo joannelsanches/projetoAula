ed8807ce0872d401e47d1cc4aee7288a9d7bf178
joannelsanches <joannesanches.bg013@academico.ifsul.edu.br>
2025-08-21 10:58:30 -0300

    iso

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import routes from './routes/route.js'; // rotas externas
+import alunoRoutes from './routes/AlunoRoutes.js';
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.static(join(__dirname, '/public')));
 app.set('views', join(__dirname, '/views'));
 
 // Rotas
+app.use(alunoRoutes)
 app.use(routes)
 app.listen(3001)
 // Exporta o handler compatível com Vercel
